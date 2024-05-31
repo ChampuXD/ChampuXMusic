f1823b57db7849f1b73345d1b82ffb0383d86cf0
Champu <112340896+ChampuXD@users.noreply.github.com>
2024-06-01 00:00:30 +0530

    done

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import re
+import os
 from os import getenv
-
 from dotenv import load_dotenv
 from pyrogram import filters
 
@@ -13,16 +13,20 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 
+#Put your bot's username
+BOT_USERNAME = getenv("BOT_USERNAME" , None)
+
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOGGER_ID = int(getenv("LOGGER_ID"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 
-# Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+# Get this value from  on Telegram by /id
+OWNER_ID = int(getenv("OWNER_ID"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -40,50 +44,50 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TheChampu")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TheChampuClub")
-
-# Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FenuZone")
 
 # Maximum Limit Allowed for users to save playlists on bot's server
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "100"))
 
-
+# MaximuM limit for fetching playlist's track from youtube, spotify, apple links.
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "100"))
+# Set this to True if you want the assistant to automatically leave chats after an interval
+AUTO_LEAVING_ASSISTANT = False
 
 # Get this credentials from https://developer.spotify.com/dashboard
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "19609edb1b9f4ed7be0c8c1342039362")
+SPOTIFY_CLIENT_SECRET = getenv(
+    "SPOTIFY_CLIENT_SECRET", "409e31d3ddd64af08cfcc3b0f064fcbe"
+)
 
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 100))
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 2500))
 
 
 # Telegram audio and video file size limit (in bytes)
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
+# Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
 # Time after which bot will suggest random chats about bot commands.
 AUTO_SUGGESTION_TIME = int(
     getenv("AUTO_SUGGESTION_TIME", "3")
 )  # Remember to give value in Seconds
 
-# Set it True if you want to bot to suggest about bot commands to random
-# chats of your bots.
+# Set it True if you want to bot to suggest about bot commands to random chats of your bots.
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
 # Cleanmode time after which bot will delete its old messages from chats
 CLEANMODE_DELETE_MINS = int(
     getenv("CLEANMODE_MINS", "5")
 )  # Remember to give value in Seconds
 
-
-# Get your pyrogram v2 session from @ChampuStringBot on Telegram
+# Get your pyrogram v2 session from @ChampuStringBOT on Telegram
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
-
 
 BANNED_USERS = filters.user()
 adminlist = {}
@@ -91,24 +95,28 @@ lyrical = {}
 votemode = {}
 autoclean = []
 confirmer = {}
+chatstats = {}
+userstats = {}
+clean = {}
 
+autoclean = []
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/ab33ea36f3c6d583575fd.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/55ea468498cfb7881c28b.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/97a58a8b3934fe6151399.jpg"
+    "PING_IMG_URL", "https://graph.org/file/15fde98db07a70beb6f4a.jpg"
 )
-PLAYLIST_IMG_URL = "https://graph.org/file/d7224f80ca0c9eb1509fe.jpg"
-STATS_IMG_URL = "https://graph.org/file/7ae0b58a0856e58156b02.jpg"
-TELEGRAM_AUDIO_URL = "https://graph.org/file/b68bb51a3ba3e25b233f8.jpg"
-TELEGRAM_VIDEO_URL = "https://graph.org/file/056bd25c84b4c15e2a58c.jpg)"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://graph.org/file/9e8ae94f6fd4749eee44e.jpg)"
-YOUTUBE_IMG_URL = "https://graph.org/file/7db91578aefff0b38e26e.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/d885c3fe9d162ab03a4f8.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/9e8ae94f6fd4749eee44e.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/7ae0b58a0856e58156b02.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/8ff7a386f161aea9ed5fb.jpg"
+STATS_IMG_URL = "https://graph.org/file/2dcb664a9c0ba9d9d80f6.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/b13a16734bab174f58482.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/5938774f48c1f019c73f7.jpg"
+STREAM_IMG_URL = "https://graph.org/file/61b2679bd92a3ab646153.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/7aed421dbfbad17f0469f.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/7e177561e54188f35fa03.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/42739cf35a58f1eda76f0.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/9553b762fd6a2aaf7ab0a.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/08b0f34c8012e2e231978.jpg"
 
 
 def time_to_seconds(time):
