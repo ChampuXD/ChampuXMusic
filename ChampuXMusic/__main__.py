7ccd920748c62b0d3415b2280b2f1073d743d71a
Champu <112340896+ChampuXD@users.noreply.github.com>
2024-05-31 22:47:17 +0530

    fix

--- ChampuXMusic/__main__.py
+++ ChampuXMusic/__main__.py
@@ -2,15 +2,15 @@ import asyncio
 import importlib
 
 from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from config import BANNED_USERS
 from ChampuXMusic import LOGGER, app, userbot
 from ChampuXMusic.core.call import Champu
 from ChampuXMusic.misc import sudo
 from ChampuXMusic.plugins import ALL_MODULES
-from ChampuXMusic.plugins.tools.clone import restart_bots
 from ChampuXMusic.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
 
 
 async def init():
@@ -21,10 +21,8 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error(
-            "𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 V2 𝐒𝐞𝐬𝐬𝐢𝐨𝐧🤬"
-        )
-
+        LOGGER(__name__).error("ᴀssɪsᴛᴀɴᴛ ᴄʟɪᴇɴᴛ ᴠᴀʀɪᴀʙʟᴇs ɴᴏᴛ ᴅᴇғɪɴᴇᴅ, ᴇxɪᴛɪɴɢ...")
+        exit()
     await sudo()
     try:
         users = await get_gbanned()
@@ -32,31 +30,33 @@ async def init():
             BANNED_USERS.add(user_id)
         users = await get_banned_users()
         for user_id in users:
-            if user_id not in BANNED_USERS:
-                BANNED_USERS.add(user_id)
+            BANNED_USERS.add(user_id)
     except:
         pass
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("ChampuXMusic.plugins" + all_module)
-    LOGGER("ChampuXMusic.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
-
+    LOGGER("ChampuXMusic.plugins").info("sᴜᴄᴄᴇssғᴜʟʟʏ ɪᴍᴘᴏʀᴛᴇᴅ ᴍᴏᴅᴜʟᴇs...")
     await userbot.start()
-
     await Champu.start()
+    try:
+        await Champu.stream_call("https://graph.org/file/9150c41e51eb46fd61e0b.mp4")
+    except NoActiveGroupCall:
+        LOGGER("ChampuXMusic").error(
+            "ᴘʟᴇᴀsᴇ ᴛᴜʀɴ ᴏɴ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴏғ ʏᴏᴜʀ ʟᴏɢ ɢʀᴏᴜᴘ\ᴄʜᴀɴɴᴇʟ.\n\ɴsᴛᴏᴘᴘɪɴɢ ʙᴏᴛ..."
+        )
+        exit()
+    except:
+        pass
     await Champu.decorators()
-    await restart_bots()
-    LOGGER("ChampuXMusic").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    LOGGER("ChampuXMusic").info(
+        "\x43\x68\x61\x6D\x70\x75\x20\x42\x6F\x74\x20\x68\x61\x73\x20\x62\x65\x65\x6E\x20\x73\x75\x63\x63\x65\x73\x73\x66\x75\x6C\x6C\x79\x20\x73\x74\x61\x72\x74\x65\x64\x2E\x0A\x0A\x40\x54\x68\x65\x43\x68\x61\x6D\x70\x75\x20"
+    )
     await idle()
-
     await app.stop()
     await userbot.stop()
-
-    LOGGER("ChampuXMusic").info(
-        "                 ╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝"
-    )
+    LOGGER("ChampuXMusic").info("sᴛᴏᴘᴘɪɴɢ ᴄʜᴀᴍᴘᴜ ᴍᴜsɪᴄ ʙᴏᴛ...")
 
 
 if __name__ == "__main__":
-
     asyncio.get_event_loop().run_until_complete(init())
